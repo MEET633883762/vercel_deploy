55076a09e02983bf678f090fcbb9fc2224e2741e
MEET VERMA <vermameet520@gmail.com>
2026-02-07 04:03:59 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://nonforbearingly-unretaining-jannie.ngrok-free.dev/",
   timeout: 30000,
 });
+
